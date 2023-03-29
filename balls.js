fa260253a4fe723cc2e5ef2cbd3b4870c4d05834
FREEDOM <vinxad@gmail.com>
2023-03-29 23:49:32 +0200

    fixed momentum increasing after collision, set defaults, fixed some silent overflows

--- balls.js
+++ balls.js
@@ -13,9 +13,9 @@ class Ball {
 
     //static maxVelocity = 1000;
     static gravity = {x: 0, y: 0}
-    static drag = 0.1;
-    static restitution = 0.98; // 1 = no loss of energy (100% elastic), 0 = no bounce
-    static rho = 0.1;
+    static drag = 0.0;
+    static restitution = 1; // 1 = no loss of energy (100% elastic), 0 = no bounce
+    static rho = 0.0;
 
     static horizontalCollisionLine = null;
     static verticalCollisionLine = null;
@@ -33,16 +33,18 @@ class Ball {
 
         this.#radius = radius;
 
-        this.#mass = (4 / 3) * Math.PI * Math.pow(this.#radius, 3);
+        //this.#mass = (4 / 3) * Math.PI * Math.pow(this.#radius, 3);
         //this.#mass = Math.PI * Math.pow(this.#radius, 3);
-        //this.#mass = this.#radius ** 2;
+        this.#mass = this.#radius * 5;
     }
 
     update(dt) {
         this.updatePosition(dt);
-        this.updateVelocity(dt);
-        this.solveCollision(dt);
         this.constrain();
+        this.solveCollision(dt);
+        this.updateVelocity(dt);
+        //this.solveCollision(dt);
+        //this.constrain();
     }
 
     newAcceleration() {
@@ -63,8 +65,13 @@ class Ball {
         this.#velocityX += this.#accelerationX * dt * 0.5;
         this.#velocityY += this.#accelerationY * dt * 0.5;
 
-        this.#accelerationX = 0;
-        this.#accelerationY = 0;
+        if (Math.abs(this.#velocityX) < 0.00000001)
+            this.#velocityX = 0;
+        if (Math.abs(this.#velocityY) < 0.00000001)
+            this.#velocityY = 0;
+
+        //this.#accelerationX = 0;
+        //this.#accelerationY = 0;
     }
 
     updatePosition(dt) {
@@ -96,7 +103,11 @@ class Ball {
     }
 
     momentum(m1, m2, v1, v2) {
-        return m1 * v1 + m2 * v2;
+        return Math.trunc(m1 * Math.abs(v1) + m2 * Math.abs(v2));
+    }
+
+    getMass() {
+        return this.#mass;
     }
 
     getVelocity() {
@@ -113,6 +124,9 @@ class Ball {
 
     setRadius(radius) {
         this.#radius = radius;
+        //this.#mass = (4 / 3) * Math.PI * Math.pow(this.#radius, 3);
+        //this.#mass = Math.PI * Math.pow(this.#radius, 3);
+        this.#mass = this.#radius * 5;
     }
 
     // Dot product
@@ -121,13 +135,19 @@ class Ball {
     }
 
     solveCollision() {
+        const educational = educ;
         const [v1x, v1y, thisPosX, thisPosY, m1] = [this.#velocityX, this.#velocityY, this.#positionX, this.#positionY, this.#mass];
+        const elastic = Ball.restitution === 1 && Ball.rho === 0 && Ball.drag === 0;
         for (let ball of balls) {
             if (ball === this) continue;
             const dx = thisPosX - ball.#positionX;
             const dy = thisPosY - ball.#positionY;
             const dsqr = dx * dx + dy * dy;
             if (dsqr < (this.#radius + ball.#radius) ** 2) {
+                // Momentum before collision
+                const momentX = this.momentum(m1, ball.#mass, v1x, ball.#velocityX);
+                const momentY = this.momentum(m1, ball.#mass, v1y, ball.#velocityY);
+
                 const theta = Math.atan2(dy, dx);
                 const col_vec = [Math.cos(theta), Math.sin(theta)];
                 const col_vec_perp = [-Math.sin(theta), Math.cos(theta)];
@@ -138,8 +158,8 @@ class Ball {
                 const v1_perp = this.dot([v1x, v1y], col_vec_perp);
                 const v2_perp = this.dot([ball.#velocityX, ball.#velocityY], col_vec_perp);
 
-                const v1_col_f = ((m1 - m1) * v1_col + 2 * m1 * v2_col) / (m1 + m1);
-                const v2_col_f = ((m1 - m1) * v2_col + 2 * m1 * v1_col) / (m1 + m1);
+                const v1_col_f = ((ball.#mass - m1) * v1_col + 2 * ball.#mass * v2_col) / (m1 + ball.#mass);
+                const v2_col_f = ((m1 - ball.#mass) * v2_col + 2 * m1 * v1_col) / (m1 + ball.#mass);
 
                 const v1_f = [v1_col_f * col_vec[0] + v1_perp * col_vec_perp[0], v1_col_f * col_vec[1] + v1_perp * col_vec_perp[1]];
                 const v2_f = [v2_col_f * col_vec[0] + v2_perp * col_vec_perp[0], v2_col_f * col_vec[1] + v2_perp * col_vec_perp[1]];
@@ -150,12 +170,23 @@ class Ball {
                 ball.#velocityX = v2_f[0] * Ball.restitution;
                 ball.#velocityY = v2_f[1] * Ball.restitution;
 
-                Ball.verticalCollisionLine = (this.#positionX * ball.#radius + ball.#positionX * this.#radius) / (this.#radius + ball.#radius);
-                Ball.horizontalCollisionLine = (this.#positionY * ball.#radius + ball.#positionY * this.#radius) / (this.#radius + ball.#radius);
+                // Momentum after the collision
+                const momentX2 = this.momentum(m1, ball.#mass, this.#velocityX, ball.#velocityX);
+                const momentY2 = this.momentum(m1, ball.#mass, this.#velocityY, ball.#velocityY);
+                if ((elastic && momentX2+momentY2 !== momentX+momentY) || (momentX2+momentY2 > momentX+momentY)) {
+                    const ratio = (momentX+momentY)/(momentX2+momentY2);
+                    this.#velocityX *= ratio;
+                    this.#velocityY *= ratio;
+                    ball.#velocityX *= ratio;
+                    ball.#velocityY *= ratio;
+                }
 
-                // Set collision output to balls direction
-                Ball.collisionOutput1 = [this.#velocityX, this.#velocityY];
-                Ball.collisionOutput2 = [ball.#velocityX, ball.#velocityY];
+                if (educational) {
+                    Ball.verticalCollisionLine = (this.#positionX * ball.#radius + ball.#positionX * this.#radius) / (this.#radius + ball.#radius);
+                    Ball.horizontalCollisionLine = (this.#positionY * ball.#radius + ball.#positionY * this.#radius) / (this.#radius + ball.#radius);
+                    Ball.collisionOutput1 = [this.#velocityX, this.#velocityY];
+                    Ball.collisionOutput2 = [ball.#velocityX, ball.#velocityY];
+                }
 
                 const overlap = (this.#radius + ball.#radius) - Math.sqrt(dsqr);
                 this.#positionX += col_vec[0] * overlap;
