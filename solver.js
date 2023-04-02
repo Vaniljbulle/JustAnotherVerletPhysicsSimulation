dd6a960932c57f5f3f26e9ad9db2b1ce37cc5e78
FREEDOM <vinxad@gmail.com>
2023-04-02 19:52:21 +0200

    Added links and link constraint

--- solver.js
+++ solver.js
@@ -2,12 +2,16 @@ import {Ball} from './ball.js';
 
 export class Solver {
     #balls;
+    links;
+    cWidth = 0;
+    cHeight = 0;
     #context;
     #gravity;
 
     static #elasticity = 1;
     static store = false;
     static vector = false;
+
 
     #collisionNormal = null;
     #collisionPoint = null;
@@ -16,13 +20,19 @@ export class Solver {
 
     constructor(ctx, [gravityX, gravityY] = [0, 0]) {
         this.#balls = [];
+        this.links = [];
         this.#gravity = [gravityX, gravityY];
         this.#context = ctx;
     }
 
     // Add new ball to the simulation
-    addBall([x,y] = [0,0], [vx, vy] = [0,0], radius = 10, color = 'black') {
-        this.#balls.push(new Ball([x,y], [vx, vy], radius, color));
+    addBall([x, y] = [0, 0], [vx, vy] = [0, 0], radius = 10, color = 'black') {
+        this.#balls.push(new Ball([x, y], [vx, vy], radius, color));
+    }
+
+    // Add new link to the simulation
+    addLink(ball1, ball2, length = 100, strength = 0.1) {
+        this.links.push({ball1, ball2, length, strength});
     }
 
     // Remove ball from the simulation
@@ -30,8 +40,25 @@ export class Solver {
         this.#balls = this.#balls.filter(b => b !== ball);
     }
 
+    removeLink(link) {
+        this.links = this.links.filter(l => l !== link);
+    }
+
+    destroyLink(index) {
+        this.links = this.links.filter(l => l.ball1 !== index && l.ball2 !== index);
+        this.removeBall(index)
+    }
+
     reset() {
         this.#balls = [];
+        this.links = [];
+        this.cWidth = 0;
+        this.cHeight = 0;
+        this.drag = 0;
+        this.store = false;
+        this.vector = false;
+        this.gravity = [0, 0];
+        this.elasticity = 1;
     }
 
     ballLength() {
@@ -41,9 +68,11 @@ export class Solver {
     set elasticity(value) {
         Solver.#elasticity = value;
     }
+
     set gravity([x, y]) {
         this.#gravity = [x, y];
     }
+
     get gravity() {
         return this.#gravity;
     }
@@ -70,16 +99,187 @@ export class Solver {
         }
     }
 
+    // Update with links
+    updateLinks(dt) {
+        for (let ball of this.#balls) {
+            ball.applyForce([ball.mass * this.#gravity[0], ball.mass * this.#gravity[1]]);
+            ball.update(dt);
+            this.constrain(ball);
+        }
+        for (let link of this.links) {
+            this.constrainLink(link);
+        }
+    }
+
+    getBall(index) {
+        return this.#balls[index];
+    }
+
+    moveBall(index, [x, y]) {
+        this.#balls[index].position = [x, y];
+    }
+
+    fixed(index, value) {
+        this.#balls[index].fixed = value;
+    }
+
+    clothInput(width, height) {
+        this.cWidth = width;
+        this.cHeight = height;
+        // Add new ball
+        for (let j = 0; j < height; j++) {
+            for (let i = 0; i < width; i++) {
+                this.addBall([i * 10 + 200, j * 10 + 100], [0, 0], 10, `rgba(0, 0, 0, 0)`);
+            }
+        }
+
+        // Add new link horizontally
+        for (let i = 0; i < height; i++) {
+            for (let j = 0; j < width - 1; j++) {
+                const ball1 = this.#balls[i * width + j];
+                const ball2 = this.#balls[i * width + j + 1];
+                this.addLink(ball1, ball2, 10, 0.8);
+            }
+        }
+        for (let j = 0; j < width; j++) {
+            const ball1 = this.#balls[j];
+            const ball2 = this.#balls[width + j];
+            this.addLink(ball1, ball2, 10, 2);
+        }
+        for (let i = 1; i < height - 1; i++) {
+            for (let j = 0; j < width; j++) {
+                const ball1 = this.#balls[i * width + j];
+                const ball2 = this.#balls[(i + 1) * width + j];
+                this.addLink(ball1, ball2, 10, 0.8);
+            }
+        }
+        /*
+        // Add new link vertically
+        for (let j = 0; j < width; j++) {
+            const ball1 = this.#balls[j];
+            const ball2 = this.#balls[width + j];
+            this.addLink(ball1, ball2, 5, 2);
+        }
+        let strength = 1
+        let length = 3;
+        for (let i = 1; i < height - 1; i++) {
+            for (let j = 0; j < width; j++) {
+                const ball1 = this.#balls[i * width + j];
+                const ball2 = this.#balls[(i + 1) * width + j];
+                this.addLink(ball1, ball2, length, strength);
+            }
+            strength *= 0.98;
+            length *= 1.01;
+        }
+*/
+        for (let i = 0; i < width; i++) {
+            this.fixed(i, true);
+        }
+    }
+
+    renderLink(link) {
+        const [x1, y1] = link.ball1.position;
+        const [x2, y2] = link.ball2.position;
+        this.#context.beginPath();
+        this.#context.moveTo(x1, y1);
+        this.#context.lineTo(x2, y2);
+        this.#context.strokeStyle = 'red';
+        this.#context.stroke();
+    }
+
+
+    renderCloth(pos1, pos2, pos3, pos4, color) {
+        const [x1, y1] = pos1;
+        const [x2, y2] = pos2;
+        const [x3, y3] = pos3;
+        const [x4, y4] = pos4;
+        this.#context.beginPath();
+        this.#context.moveTo(x1, y1);
+        this.#context.lineTo(x2, y2);
+        this.#context.lineTo(x3, y3);
+        this.#context.lineTo(x4, y4);
+        this.#context.lineTo(x1, y1);
+        this.#context.closePath();
+        this.#context.fillStyle = color;
+        this.#context.fill();
+    }
+
     // Render the simulation
     render() {
         this.#context.clearRect(0, 0, this.#context.canvas.width, this.#context.canvas.height);
         this.#balls.forEach(ball => ball.paint(this.#context));
+        //this.links.forEach(link => this.renderLink(link));
+
+        if (this.cWidth !== 0) {
+            const squares = (this.cWidth - 1) * (this.cHeight - 1);
+            for (let i = 0; i < squares; i++) {
+                const rowIndex = Math.floor(i / (this.cWidth - 1));
+                let c;
+                if (rowIndex % 2 === 1) {
+                    c = 'rgba(0, 255, 0, 0.3)'
+                } else {
+                    c = 'rgba(0, 0, 255, 0.3)'
+                }
+                const top = i;
+                const bottom = i + this.cWidth - 1;
+
+                this.renderCloth(
+                    this.links[top].ball1.position,
+                    this.links[top].ball2.position,
+                    this.links[bottom].ball2.position,
+                    this.links[bottom].ball1.position,
+                    c)
+
+            }
+        }
+
+
         if (Solver.store) {
             this.renderCollisionInformation();
         }
         if (Solver.vector) {
             this.#balls.forEach(ball => this.renderVector(ball.position, ball.velocity, 'red'));
         }
+    }
+
+    constrainLink(link) {
+        const [ball1, ball2] = [link.ball1, link.ball2];
+        if (ball1.fixed && ball2.fixed) {
+            return;
+        }
+        const [x1, y1] = ball1.position;
+        const [x2, y2] = ball2.position;
+        const dx = x2 - x1;
+        const dy = y2 - y1;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        let normal = [dx / distance, dy / distance];
+        const d = link.length - distance;
+
+        /*
+        if (d < -500) {
+            this.removeBall(ball1);
+            this.removeBall(ball2);
+            this.removeLink(link);
+            return;
+        }
+*/
+        // Ball 1 is fixed
+        if (ball1.fixed) {
+            ball2.position[0] += d * normal[0] * link.strength;
+            ball2.position[1] += d * normal[1] * link.strength;
+            return;
+        }
+        // Ball 2 is fixed
+        if (ball2.fixed) {
+            ball1.position[0] -= d * normal[0] * link.strength;
+            ball1.position[1] -= d * normal[1] * link.strength;
+            return;
+        }
+        ball1.position[0] -= d * normal[0] * link.strength;
+        ball1.position[1] -= d * normal[1] * link.strength;
+        ball2.position[0] += d * normal[0] * link.strength;
+        ball2.position[1] += d * normal[1] * link.strength;
     }
 
     // Constrain the ball to the canvas
@@ -102,7 +302,78 @@ export class Solver {
         }
     }
 
-    // Iterate over all balls and check for collisions (Will be optimized at some point)
+    /*
+        collisions() {
+            // Sweep and prune
+            const minX = this.#balls.map(ball => ball.position[0] - ball.radius * 2);
+            const maxX = this.#balls.map(ball => ball.position[0] + ball.radius * 2);
+            const minY = this.#balls.map(ball => ball.position[1] - ball.radius * 2);
+            const maxY = this.#balls.map(ball => ball.position[1] + ball.radius * 2);
+
+            const sortedMinX = [...minX].sort((a, b) => a - b);
+            const sortedMinY = [...minY].sort((a, b) => a - b);
+
+            const xIndices = minX.map(x => sortedMinX.indexOf(x));
+            const yIndices = minY.map(y => sortedMinY.indexOf(y));
+
+            const xPairs = [];
+            const yPairs = [];
+
+            for (let i = 0; i < this.#balls.length; i++) {
+                for (let j = i + 1; j < this.#balls.length; j++) {
+                    if (xIndices[i] < xIndices[j] && maxX[i] >= minX[j]) {
+                        xPairs.push([i, j]);
+                    }
+                    if (yIndices[i] < yIndices[j] && maxY[i] >= minY[j]) {
+                        yPairs.push([i, j]);
+                    }
+                }
+            }
+
+            const pairs = xPairs.filter(pair => yPairs.some(p => p[0] === pair[0] && p[1] === pair[1]));
+
+            for (let pair of pairs) {
+                this.resolveCollision(this.#balls[pair[0]], this.#balls[pair[1]]);
+            }
+        }
+    */
+
+    /*
+    collisions() {
+        // Sweep and prune
+        const minX = this.#balls.map(ball => ball.position[0] - ball.radius * 2);
+        const maxX = this.#balls.map(ball => ball.position[0] + ball.radius * 2);
+        const minY = this.#balls.map(ball => ball.position[1] - ball.radius * 2);
+        const maxY = this.#balls.map(ball => ball.position[1] + ball.radius * 2);
+
+        const sortedMinX = [...minX].sort((a, b) => a - b);
+        const sortedMinY = [...minY].sort((a, b) => a - b);
+
+        const xIndices = minX.map(x => sortedMinX.indexOf(x));
+        const yIndices = minY.map(y => sortedMinY.indexOf(y));
+
+        const xPairs = [];
+        const yPairs = [];
+
+        for (let i = 0; i < this.#balls.length; i++) {
+            for (let j = i + 1; j < this.#balls.length; j++) {
+                if (xIndices[i] < xIndices[j] && maxX[i] >= minX[j]) {
+                    xPairs.push([i, j]);
+                }
+                if (yIndices[i] < yIndices[j] && maxY[i] >= minY[j]) {
+                    yPairs.push([i, j]);
+                }
+            }
+        }
+
+        const pairs = xPairs.filter(pair => yPairs.some(p => p[0] === pair[0] && p[1] === pair[1]));
+
+        for (let pair of pairs) {
+            this.resolveCollision(this.#balls[pair[0]], this.#balls[pair[1]]);
+        }
+    }
+    */
+
     collisions(ball) {
         for (let other of this.#balls) {
             if (other !== ball) {
@@ -142,13 +413,13 @@ export class Solver {
             ball1.velocity = [(vx1 - momentum * e * m2 * normal[0]), (vy1 - momentum * e * m2 * normal[1])];
             ball2.velocity = [(vx2 + momentum * e * m1 * normal[0]), (vy2 + momentum * e * m1 * normal[1])];
 
-            if(Solver.store){
+            if (Solver.store) {
                 // Store collision information for rendering
                 this.renderCollisionInformation();
                 this.#collisionPoint = [(x1 * r2 + x2 * r1) / (r1 + r2), (y1 * r2 + y2 * r1) / (r1 + r2)];
                 this.#collisionNormal = normal;
-                this.#direction1 = [ball1.velocity[0],ball1.velocity[1]];
-                this.#direction2 = [ball2.velocity[0],ball2.velocity[1]];
+                this.#direction1 = [ball1.velocity[0], ball1.velocity[1]];
+                this.#direction2 = [ball2.velocity[0], ball2.velocity[1]];
             }
 
 
@@ -162,15 +433,15 @@ export class Solver {
     }
 
     kineticEnergy(m1, v1x, v1y, m2, v2x, v2y) {
-        const p1 = 0.5 * m1 * (v1x**2 + v1y**2);
-        const p2 = 0.5 * m2 * (v2x**2 + v2y**2);
+        const p1 = 0.5 * m1 * (v1x ** 2 + v1y ** 2);
+        const p2 = 0.5 * m2 * (v2x ** 2 + v2y ** 2);
         return p1 + p2;
     }
 
     kineticEnergySystem() {
         let ke = 0;
         for (let ball of this.#balls) {
-            ke += 0.5 * ball.mass * (ball.velocity[0]**2 + ball.velocity[1]**2);
+            ke += 0.5 * ball.mass * (ball.velocity[0] ** 2 + ball.velocity[1] ** 2);
         }
         return ke;
     }
